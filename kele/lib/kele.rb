761016199baa123671306297bc2496b718a1cbc4
Annie Coleman <annierm18@yahoo.com>
2018-08-14 20:51:00 -0700

    checkpoint 2: pt2

--- kele/lib/kele.rb
+++ kele/lib/kele.rb
@@ -1,5 +1,16 @@
-require "kele/version"
+class Kele
+    include HTTParty
+    require "kele/version"
 
-module Kele
-  # Your code goes here...
+    def initialize(email, password)
+        @options = {query: {email: email, password: password}}
+        response = self.class.post("https://www.bloc.io/api/v1/sessions", @options)
+        @auth_token = response["auth_token"]
+    end
+
+
+#  module Kele
+    # Your code goes here...
+#  end
+
 end

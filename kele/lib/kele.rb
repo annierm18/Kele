3adfad2f03bb2ff7fc243704f8b38c0d9d78ab12
Annie Coleman <annierm18@yahoo.com>
2018-08-19 16:40:08 -0700

    checkpoint 3: pt 1

--- kele/lib/kele.rb
+++ kele/lib/kele.rb
@@ -9,6 +9,11 @@ class Kele
         @auth_token = response["auth_token"]
     end
 
+    def get_me
+           response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: {"authorization" => @auth_token})
+           JSON.parse(response.body)
+    end
+
 
 #  module Kele
     # Your code goes here...
